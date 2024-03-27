7299cce2de578eb51aa7c4204b229a50bb87cc00
Anthonin Bonnefoy <anthonin.bonnefoy@datadoghq.com>
2024-03-27 09:26:57 +0100

    Avoid cascade in cleanup test

--- sql/cleanup.sql
+++ sql/cleanup.sql
@@ -2,4 +2,6 @@
 DROP TABLE pg_tracing_test;
 DROP function test_function_project_set;
 DROP function test_function_result;
-DROP EXTENSION pg_tracing CASCADE;
+DROP VIEW peek_ordered_spans;
+DROP VIEW peek_spans_with_level;
+DROP EXTENSION pg_tracing;
