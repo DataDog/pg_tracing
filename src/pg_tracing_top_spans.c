420f129e0efe8a0f0c6b55dceaa63228a9a4f578
Anthonin Bonnefoy <anthonin.bonnefoy@datadoghq.com>
2024-06-06 10:23:50 +0200

    rename get_latest_top_span

--- src/pg_tracing_top_spans.c
+++ src/pg_tracing_top_spans.c
@@ -14,20 +14,6 @@
 
 
 /*
- * Get the latest span for a specific level. The span must exists
- */
-Span *
-get_latest_top_span(int nested_level)
-{
-	pgTracingSpans *top_spans;
-
-	Assert(nested_level >= 0);
-	top_spans = per_level_buffers[nested_level].top_spans;
-	Assert(top_spans->end > 0);
-	return &top_spans->spans[top_spans->end - 1];
-}
-
-/*
  * Create a new top_span for the current exec nested level
  */
 Span *
@@ -56,6 +42,40 @@ allocate_new_top_span(void)
 }
 
 /*
+ * Get the ongoing top span if it exists or create it
+ */
+Span *
+get_or_allocate_top_span(pgTracingTraceContext * trace_context, bool in_parse_or_plan)
+{
+	Span	   *span;
+
+	if (in_parse_or_plan && exec_nested_level == 0)
+
+		/*
+		 * The root post parse and plan, we want to use trace_context's
+		 * root_span as the top span in per_level_buffers might still be
+		 * ongoing.
+		 */
+		return &trace_context->root_span;
+
+	if (per_level_buffers[exec_nested_level].top_spans->end == 0)
+		/* No spans were created in this level, allocate a new one */
+		span = allocate_new_top_span();
+	else
+		span = peek_top_span();
+
+	if (exec_nested_level == 0)
+
+		/*
+		 * At root level and outside of parse/plan hook, we need to copy the
+		 * root span content
+		 */
+		*span = trace_context->root_span;
+
+	return span;
+}
+
+/*
  * Get the latest top span
  */
 Span *
@@ -64,6 +84,20 @@ peek_top_span(void)
 	pgTracingSpans *top_spans;
 
 	top_spans = per_level_buffers[exec_nested_level].top_spans;
+	Assert(top_spans->end > 0);
+	return &top_spans->spans[top_spans->end - 1];
+}
+
+/*
+ * Get the latest span for a specific level. The span must exists
+ */
+Span *
+peek_nested_level_top_span(int nested_level)
+{
+	pgTracingSpans *top_spans;
+
+	Assert(nested_level >= 0);
+	top_spans = per_level_buffers[nested_level].top_spans;
 	Assert(top_spans->end > 0);
 	return &top_spans->spans[top_spans->end - 1];
 }
