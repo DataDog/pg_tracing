c5043cc2030e0dd7415ef3c9a4506d0cc7f57f6b
Anthonin Bonnefoy <anthonin.bonnefoy@datadoghq.com>
2024-06-18 11:28:51 +0200

    Add more tests for extended protocol

--- sql/extended.sql
+++ sql/extended.sql
@@ -18,6 +18,11 @@ SELECT $1 \bind 1 \g
 SELECT $1, $2 \bind 2 3 \g
 COMMIT;
 
+-- Send begin through extended protocol
+BEGIN \bind \g
+SELECT $1 \bind 1 \g
+COMMIT;
+
 -- Spans within the same transaction should have been generated with the same trace_id
 SELECT count(distinct(trace_id)) = 1 FROM pg_tracing_peek_spans;
 SELECT span_type, span_operation, parameters, lvl FROM peek_ordered_spans;
@@ -70,6 +75,12 @@ SELECT $1 \bind 1 \g
 COMMIT;
 
 SELECT span_type, span_operation, parameters, lvl FROM peek_ordered_spans WHERE trace_id='00000000000000000000000000000003';
+
+-- Test tracing the whole transaction with extended protocol with BEGIN sent through extended protocol
+/*dddbs='postgres.db',traceparent='00-00000000000000000000000000000004-0000000000000004-01'*/ BEGIN \bind \g
+SELECT $1 \bind 1 \g
+COMMIT;
+SELECT span_type, span_operation, parameters, lvl FROM peek_ordered_spans WHERE trace_id='00000000000000000000000000000004';
 
 -- Cleanup
 CALL clean_spans();
