42ef5d93951935c1e0fa535011b28d22584e203c
Anthonin Bonnefoy <anthonin.bonnefoy@datadoghq.com>
2024-07-02 09:27:16 +0200

    Add subxact_count test for json export

--- sql/json.sql
+++ sql/json.sql
@@ -11,8 +11,16 @@ CALL clean_spans();
 set statement_timeout=200;
 /*dddbs='postgres.db',traceparent='00-00000000000000000000000000000001-0000000000000001-01'*/ select * from pg_sleep(1);
 -- Check json generated spans with sql error code
-SELECT trace_id, name, kind, sql_error_code, lvl FROM peek_ordered_json_spans;
+SELECT trace_id, name, sql_error_code, lvl FROM peek_ordered_json_spans;
 set statement_timeout TO DEFAULT;
+CALL clean_spans();
+
+-- Test subxact_count with json export
+/*dddbs='postgres.db',traceparent='00-00000000000000000000000000000001-0000000000000001-01'*/ BEGIN;
+SAVEPOINT s1;
+INSERT INTO pg_tracing_test VALUES(generate_series(1, 2), 'aaa');
+ROLLBACK;
+SELECT trace_id, name, subxact_count, lvl FROM peek_ordered_json_spans;
 
 
 -- Cleanup
