fb839aa61966ba4bf9ee9526df0e3c953b06596b
Anthonin Bonnefoy <anthonin.bonnefoy@datadoghq.com>
2024-07-02 10:25:16 +0200

    Fix sql_error_code test with json export

--- sql/json.sql
+++ sql/json.sql
@@ -7,15 +7,14 @@
 SELECT trace_id, name, kind, lvl FROM peek_ordered_json_spans;
 -- Test plan attributes with json export
 SELECT trace_id, name, plan_startup_cost, plan_total_cost, plan_rows, plan_width, lvl FROM peek_ordered_json_spans;
-
 CALL clean_spans();
 
 -- Test error code with json export
 set statement_timeout=200;
 /*dddbs='postgres.db',traceparent='00-00000000000000000000000000000001-0000000000000001-01'*/ select * from pg_sleep(1);
 -- Check json generated spans with sql error code
-SELECT trace_id, name, sql_error_code, lvl FROM peek_ordered_json_spans;
 set statement_timeout TO DEFAULT;
+SELECT trace_id, name, sql_error_code, lvl FROM peek_ordered_json_spans;
 CALL clean_spans();
 
 -- Test subxact_count with json export
