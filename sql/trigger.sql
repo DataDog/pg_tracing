43e89ca94e3c94d4b6db3af44a89fcc874251a06
Anthonin Bonnefoy <anthonin.bonnefoy@datadoghq.com>
2024-11-22 14:57:46 +0100

    Split expected pg_regress per version

--- sql/trigger.sql
+++ sql/trigger.sql
@@ -184,6 +184,11 @@ create trigger qqqbef before insert or update or delete on copydml_test
 
 SELECT trace_id, span_type, span_operation, lvl from peek_ordered_spans where trace_id='00000000000000000000000000000005';
 
+-- Test explain on a before trigger
+-- This will go through ExecutorEnd without any parent executor run
+/*dddbs='postgres.db',traceparent='00-fed00000000000000000000000000006-0000000000000003-01'*/ explain INSERT INTO before_trigger_table VALUES(10);
+SELECT trace_id, span_type, span_operation, lvl from peek_ordered_spans where trace_id='fed00000000000000000000000000006';
+
 -- Cleanup
 CALL reset_settings();
 CALL clean_spans();
--- sql/trigger_16.sql
+++
@@ -1,6 +0,0 @@
--- Test explain on a before trigger
--- This will go through ExecutorEnd without any parent executor run
-/*dddbs='postgres.db',traceparent='00-fed00000000000000000000000000001-0000000000000003-01'*/ explain INSERT INTO before_trigger_table VALUES(10);
-SELECT trace_id, span_type, span_operation, lvl from peek_ordered_spans where trace_id='fed00000000000000000000000000001';
-
-CALL clean_spans();
