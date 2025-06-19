cacb38425f50eac3edae59e9041c496efbd1ab11
Anthonin Bonnefoy <anthonin.bonnefoy@datadoghq.com>
2025-06-19 08:34:51 +0200

    Improve PG_VERSION extraction
    18beta1 wasn't handled correctly, the new regexp will capture the first
    group of number which should be the major version.

--- common.mk
+++ common.mk
@@ -8,7 +8,7 @@ PG_CONFIG  = pg_config
 PG_CONFIG_EXISTS := $(shell command -v $(PG_CONFIG) 2> /dev/null)
 ifdef PG_CONFIG_EXISTS
 # Default to pg_config's advertised version
-PG_VERSION ?= $(shell $(PG_CONFIG) --version | cut -d' ' -f2 | cut -d'.' -f1 | tr -d 'devel')
+PG_VERSION ?= $(shell $(PG_CONFIG) --version | sed 's/PostgreSQL \([0-9]*\).*/\1/g')
 else
 # pg_config is not present, let's assume we are packaging and use the latest PG version
 PG_VERSION ?= $(lastword $(PG_VERSIONS))
